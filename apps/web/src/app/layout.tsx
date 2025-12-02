1994b5a9fbc4fe6d43dccb657e1f963e93a32b50
Celo Composer <support@celo.org>
2025-12-03 02:01:46 +0300

    chore: init project

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -9,15 +9,14 @@ const inter = Inter({ subsets: ['latin'] });
 
 const appUrl = process.env.NEXT_PUBLIC_URL || "http://localhost:3000";
 
-// Embed metadata for Farcaster sharing
 const frame = {
   version: "1",
   imageUrl: `${appUrl}/opengraph-image.png`,
   button: {
-    title: "Launch delulu-v0",
+    title: "Launch Delulu",
     action: {
       type: "launch_frame",
-      name: "delulu-v0",
+      name: "delulu",
       url: appUrl,
       splashImageUrl: `${appUrl}/icon.png`,
       splashBackgroundColor: "#ffffff",
@@ -26,11 +25,11 @@ const frame = {
 };
 
 export const metadata: Metadata = {
-  title: 'delulu-v0',
-  description: 'A new Celo blockchain project',
+  title: 'Delulu',
+  description: 'A prediction market that turns social media trendy topics, oppinions and wild(delusional) goals into high stakes ',
   openGraph: {
-    title: 'delulu-v0',
-    description: 'A new Celo blockchain project',
+    title: 'delulu',
+    description: 'A prediction market that turns social media trendy topics, oppinions and wild(delusional) goals into high stakes',
     images: [`${appUrl}/opengraph-image.png`],
   },
   other: {
@@ -49,7 +48,6 @@ export default function RootLayout({
         {/* Navbar is included on all pages */}
         <div className="relative flex min-h-screen flex-col">
           <Providers>
-            <Navbar />
             <main className="flex-1">
               {children}
             </main>
