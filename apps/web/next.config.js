d6e821210031cf1c33145d2ec602130792864f5c
Celo Composer <support@celo.org>
2025-12-04 17:03:02 +0100

    feat: deloying to farcaster

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@repo/ui"], // Keep this if you are in a monorepo
   webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    return config
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
   },
 };
 
