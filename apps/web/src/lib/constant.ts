d4999b5060163a02fe4f88d9cb16fabf7e689027
Celo Composer <support@celo.org>
2026-01-12 12:44:37 +0100

    fix: contract

--- apps/web/src/lib/constant.ts
+++ apps/web/src/lib/constant.ts
@@ -5,7 +5,7 @@ const DELULU_CONTRACT_ADDRESSES = {
     "0x84217d67b85Ef028bb32B494B7477Ab8E9b2fEcd",
   mainnet_v0: "0xE03B2Bb1aA149DedEFfc91398f477bb7Ddd278aF",
   testnet_v0: "0x14C808cFf20F431Fb8E94099967b4a10eA83D870",
-  testnet: "0xcfDd3EbEad6175663dC59A7acae24BB70b604822",
+  testnet: "0xb10dFA2e2DAd8d229BF154a7Aa8f63d6E8dFB7dB",
 } as const;
 
 const isProduction =
